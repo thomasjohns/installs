feb3dcaf99e89ce1a413da1f2dde51fa54ef9c0c
Thomas Johnson <tej5@uw.edu>
2026-04-10 15:16:33 -0700

    Build neovim from source

--- tools/neovim.sh
+++ tools/neovim.sh
@@ -5,18 +5,18 @@ tool_install() {
     if is_mac; then
         ensure_brew && brew install neovim
     elif is_linux; then
-        # Use the neovim PPA for a recent version compatible with older glibc
-        if command_exists add-apt-repository; then
-            sudo add-apt-repository -y ppa:neovim-ppa/unstable
-            sudo apt-get update -qq
-            sudo apt-get install -y neovim
-        else
-            sudo apt-get update -qq
-            sudo apt-get install -y software-properties-common
-            sudo add-apt-repository -y ppa:neovim-ppa/unstable
-            sudo apt-get update -qq
-            sudo apt-get install -y neovim
-        fi
+        # Build from source to get a recent version on older glibc systems
+        sudo apt-get update -qq
+        sudo apt-get install -y ninja-build gettext cmake unzip curl build-essential
+        local build_dir
+        build_dir="$(mktemp -d)"
+        git clone --depth 1 --branch stable https://github.com/neovim/neovim.git "$build_dir"
+        cd "$build_dir"
+        # Prefer system ninja over any broken Python wrappers in ~/.local/bin
+        export PATH="/usr/bin:$PATH"
+        make CMAKE_BUILD_TYPE=Release
+        sudo make install
+        rm -rf "$build_dir"
     fi
 }
 tool_status() { std_status "neovim" "nvim"; }
